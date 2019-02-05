31a75cc78b30f5ec98aa8b9378db7513b6178bdc
Mauro Sardara <msardara@cisco.com>
2019-02-05 10:02:16 +0100

    [HICN-25] Test docs script.
    Change-Id: I37cb7b34a737323862619c198ff4e3b570217887
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -166,10 +166,64 @@ build_package() {
     echo "*******************************************************************"
     echo "*****************  BUILD COMPLETED SUCCESSFULLY *******************"
     echo "*******************************************************************"
-
-    exit 0
 }
 
 build_package
+
+DOCS_REPO_URL=${DOCS_REPO_URL:-"https://nexus.fd.io/content/sites/site"}
+PROJECT_PATH=${PROJECT_PATH:-"io/fd/hicn"}
+DOC_FILE=${DOC_FILE:-"hicn.docs.zip"}
+DOC_DIR=${DOC_DIR:-"build/lib/doc/html"}
+SITE_DIR=${SITE_DIR:-"build/doc/deploy-site/"}
+RESOURCES_DIR=${RESOURCES_DIR:-${SITE_DIR}/src/site/resources}
+MVN=${MVN:-"/opt/apache/maven/bin/mvn"}
+VERSION=${VERSION:-$(git describe --abbrev=0 | egrep -o "([0-9]{1,}\.)+[0-9]{1,}")}
+
+echo "Current directory: $(pwd)"
+
+mkdir -p build
+pushd build
+cmake -DBUILD_HICNPLUGIN=OFF -DBUILD_HICNLIGHT=OFF -DBUILD_LIBTRANSPORT=OFF -DBUILD_UTILS=OFF ..
+make doc
+popd
+
+# if [[ ${JOB_NAME} == *merge* ]]; then
+  mkdir -p $(dirname ${RESOURCES_DIR})
+  mv -f ${DOC_DIR} ${RESOURCES_DIR}
+  cd ${SITE_DIR}
+  find . -type f '(' -name '*.md5' -o -name '*.dot' -o -name '*.map' ')' -delete
+  cat > pom.xml << EOF
+  <project xmlns="http://maven.apache.org/POM/4.0.0" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://maven.apache.org/POM/4.0.0 http://maven.apache.org/xsd/maven-4.0.0.xsd">
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>io.fd.hicn</groupId>
+    <artifactId>docs</artifactId>
+    <version>1.0.0</version>
+    <packaging>pom</packaging>
+
+    <properties>
+      <generateReports>false</generateReports>
+    </properties>
+
+    <build>
+      <extensions>
+        <extension>
+          <groupId>org.apache.maven.wagon</groupId>
+           <artifactId>wagon-webdav-jackrabbit</artifactId>
+           <version>2.9</version>
+        </extension>
+      </extensions>
+    </build>
+    <distributionManagement>
+      <site>
+        <id>fdio-site</id>
+        <url>dav:${DOCS_REPO_URL}/${PROJECT_PATH}/${VERSION}</url>
+      </site>
+    </distributionManagement>
+  </project>
+EOF
+#   ${MVN} site:site site:deploy -gs "${GLOBAL_SETTINGS_FILE}" -s "${SETTINGS_FILE}" -T 4C
+#   cd -
+# fi
+
 
 exit 0
