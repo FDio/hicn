ec0c0d12b910affdc43e6108f6856713d2f32725
Alberto Compagno <acompagn+fdio@cisco.com>
2019-02-07 13:12:19 +0100

    [HICN-39] Added api that return a pointer to the signature hold in a packet [HICN-40] Fixed signature calculation by allocating a contiguous portion of memory that holds the entire hICN header (IP+TCP+AH)
    Change-Id: I9d40bab0e3ecb82949b8b3a00e2cc1214457e4e3
    Signed-off-by: Alberto Compagno <acompagn+fdio@cisco.com>

--- libtransport/src/hicn/transport/core/packet.h
+++ libtransport/src/hicn/transport/core/packet.h
@@ -133,6 +133,8 @@ class Packet : public std::enable_shared_from_this<Packet> {
 
   std::size_t getSignatureSize() const;
 
+  uint8_t * getSignature() const;
+
   void setSignatureTimestamp(const uint64_t &timestamp);
 
   uint64_t getSignatureTimestamp() const;
