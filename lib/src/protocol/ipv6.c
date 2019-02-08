10bd8836e10e2918c24f585febdf67b773eba33f
Angelo Mantellini <manangel@cisco.com>
2019-02-08 15:11:57 +0100

    [HICN-44] Remove warnings libhicn (lib) on windows
    Change-Id: I637e9c1e05de8a9e6743ea729b62d3eedd6ca54b
    Signed-off-by: Angelo Mantellini <manangel@cisco.com>

--- lib/src/protocol/ipv6.c
+++ lib/src/protocol/ipv6.c
@@ -341,7 +341,7 @@ ipv6_set_payload_length (hicn_type_t type, hicn_protocol_t * h,
   int rc = CHILD_OPS (get_header_length, type, h, &child_header_length);
   if (rc < 0)
     return rc;
-  h->ipv6.len = htons (payload_length + child_header_length);
+  h->ipv6.len = htons ( (u_short) (payload_length + child_header_length));
   return HICN_LIB_ERROR_NONE;
 }
 
