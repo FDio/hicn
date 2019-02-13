ff8d625341caca1872416dd45ca100987f91ced8
Angelo Mantellini <manangel@cisco.com>
2019-02-13 17:27:33 +0100

    [HICN-61] Compile Error in libtransport in windows environment
    Change-Id: I25642a194996e449b91d492b22a379466c524940
    Signed-off-by: Angelo Mantellini <manangel@cisco.com>

--- libtransport/src/hicn/transport/utils/signer.cc
+++ libtransport/src/hicn/transport/utils/signer.cc
@@ -129,8 +129,8 @@ void Signer::sign(Packet &packet) {
 
   utils::CryptoHash hash = hasher.finalize();
 
-  PARCSignature *signature = parcSigner_SignDigest(
-      this->signer_, hash.hash_, packet.getSignature(), sign_len_bytes);
+  PARCSignature *signature = parcSigner_SignDigestNoAlloc(
+      this->signer_, hash.hash_, packet.getSignature(), (uint32_t)sign_len_bytes);
   PARCBuffer *buffer = parcSignature_GetSignature(signature);
 
   size_t bytes_len = parcBuffer_Remaining(buffer);
