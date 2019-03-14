e4c1167d9cda769c1b75ffd4f512432ce1663839
Mauro Sardara <msardara@cisco.com>
2019-03-14 16:03:00 +0100

    [HICN-113] Fix packaging system.
    Change-Id: I779bfffedac5c593de9789b79218475ec4698c01
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -24,15 +24,15 @@ PACKAGECLOUD_RELEASE_REPO_RPM="https://packagecloud.io/install/repositories/fdio
 VPP_GIT_REPO="https://git.fd.io/vpp"
 VPP_BRANCH="stable/1901"
 
-VPP_VERSION_DEB="19.01-release"
-VPP_VERSION_RPM="19.01-release.x86_64"
+VPP_VERSION_DEB="19.01.1-release"
+VPP_VERSION_RPM="19.01.1-release.x86_64"
 
 BUILD_TOOLS_UBUNTU="build-essential doxygen"
 LIBSSL_LIBEVENT_UBUNTU="libevent-dev libssl-dev"
 DEPS_UBUNTU="libparc-dev libasio-dev vpp-dev=${VPP_VERSION_DEB} vpp-lib=${VPP_VERSION_DEB}"
 
 # BUILD_TOOLS_GROUP_CENTOS="'Development Tools'"
-DEPS_CENTOS="vpp-devel-${VPP_VERSION_RPM} vpp-lib-${VPP_VERSION_RPM} libparc-devel asio-devel centos-release-scl devtoolset-7"
+DEPS_CENTOS="vpp-devel=${VPP_VERSION_RPM} vpp-lib=${VPP_VERSION_RPM} libparc-devel asio-devel centos-release-scl devtoolset-7"
 LATEST_EPEL_REPO="http://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm"
 
 install_cmake() {
@@ -112,7 +112,6 @@ setup() {
         # echo ${BUILD_TOOLS_GROUP_CENTOS} | xargs sudo yum groupinstall -y --nogpgcheck
         echo ${DEPS_CENTOS} | xargs sudo yum install -y --nogpgcheck
         sudo yum install devtoolset-7
-        scl enable devtoolset-7 bash
 
         c++ --version
 
