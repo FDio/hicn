8aa9151a16424c5224bd7caefc35f85e772607ca
Mauro Sardara <msardara@cisco.com>
2019-03-15 11:03:52 +0100

    [HICN-118] Add .clang-format file, format all libtransport project.
    Change-Id: I61392567b648014eaa111fc2567770192fccffa6
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ libtransport/format_all.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -xue
+
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+
+pushd ${SCRIPT_PATH}
+find src/ -type f '(' -name '*.cc' -o -name '*.h' ')' -exec clang-format -style=file -i {} \;
+popd
