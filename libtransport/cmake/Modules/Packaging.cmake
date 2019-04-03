fc938965a323d0865faf1fc93e375cdfbb66aa7b
Mauro Sardara <msardara@cisco.com>
2019-04-03 10:25:29 +0200

    [HICN-162] Resolved warning about version comparison when installing hicn-plugin from deb
    Change-Id: Ia4e6efccbff78fa9fd352f3f9b516d627fa1ff58
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- libtransport/cmake/Modules/Packaging.cmake
+++ libtransport/cmake/Modules/Packaging.cmake
@@ -32,7 +32,7 @@ set(lib${LIBTRANSPORT}-devel_DESCRIPTION ${lib${LIBTRANSPORT}_DESCRIPTION}
 if ((BUILD_MEMIF_CONNECTOR OR BUILD_HICNPLUGIN) AND "${CMAKE_SYSTEM_NAME}" STREQUAL "Linux")
 
   set(lib${LIBTRANSPORT}_DEB_DEPENDENCIES
-    "lib${LIBHICN} (>= stable_version), libparc (>= 1.0), vpp-lib (>= stable_version-release), vpp-lib (< next_version-release)"
+    "lib${LIBHICN} (>= stable_version), libparc (>= 1.0), vpp-lib (>= stable_version-release), vpp-lib (<< next_version-release)"
     CACHE STRING "Dependencies for deb/rpm package."
   )
 
@@ -42,7 +42,7 @@ if ((BUILD_MEMIF_CONNECTOR OR BUILD_HICNPLUGIN) AND "${CMAKE_SYSTEM_NAME}" STREQ
   )
 
   set(lib${LIBTRANSPORT}-dev_DEB_DEPENDENCIES
-    "lib${LIBTRANSPORT} (>= stable_version), libasio-dev (>= 1.10), libhicn-dev (>= stable_version), libparc-dev (>= 1.0), vpp-dev (>= stable_version-release), vpp-dev (< next_version-release)"
+    "lib${LIBTRANSPORT} (>= stable_version), libasio-dev (>= 1.10), libhicn-dev (>= stable_version), libparc-dev (>= 1.0), vpp-dev (>= stable_version-release), vpp-dev (<< next_version-release)"
     CACHE STRING "Dependencies for deb/rpm package."
   )
 
