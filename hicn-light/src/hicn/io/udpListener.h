198eec79dda3cc1711cb06c71831e1001555b188
Angelo Mantellini <manangel@cisco.com>
2019-07-12 08:48:59 -0700

    [HICN-245] Bind of the listener to the device
    Change-Id: I4d8cd95ba74c5d24d4c49975e7798440fe3e34d1
    Signed-off-by: Angelo Mantellini <manangel@cisco.com>

--- hicn-light/src/hicn/io/udpListener.h
+++ hicn-light/src/hicn/io/udpListener.h
@@ -27,9 +27,16 @@
 struct udp_listener;
 typedef struct udp_listener UdpListener;
 
+#ifdef __linux__
+ListenerOps *udpListener_CreateInet6(Forwarder *forwarder,
+                                     struct sockaddr_in6 sin6, const char *if_bind);
+ListenerOps *udpListener_CreateInet(Forwarder *forwarder,
+                                    struct sockaddr_in sin, const char *if_bind);
+#else
 ListenerOps *udpListener_CreateInet6(Forwarder *forwarder,
                                      struct sockaddr_in6 sin6);
 ListenerOps *udpListener_CreateInet(Forwarder *forwarder,
                                     struct sockaddr_in sin);
+#endif
 // void udpListener_SetPacketType(ListenerOps *ops, MessagePacketType type);
 #endif  // udpListener_h
