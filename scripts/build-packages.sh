d0f97027738ec6a1e1de7a456c4e4a1667779207
Mauro Sardara <msardara@cisco.com>
2019-07-26 17:32:48 +0200

    [HICN-253] Mark cmake target for library as static or shared.
    Change-Id: I1e8a14f9255f04bddbb87f74a6d6163a02dedb22
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- scripts/build-packages.sh
+++ scripts/build-packages.sh
@@ -24,8 +24,8 @@ PACKAGECLOUD_RELEASE_REPO_RPM="https://packagecloud.io/install/repositories/fdio
 VPP_GIT_REPO="https://git.fd.io/vpp"
 VPP_BRANCH="stable/1904"
 
-VPP_VERSION_DEB="19.04.1-release"
-VPP_VERSION_RPM="19.04.1-release.x86_64"
+VPP_VERSION_DEB="19.04.2-release"
+VPP_VERSION_RPM="19.04.2-release.x86_64"
 
 BUILD_TOOLS_UBUNTU="build-essential doxygen"
 LIBSSL_LIBEVENT_UBUNTU="libevent-dev libssl-dev"
