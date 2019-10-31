d17992cbce196604160af834328ef4e9939a2671
masoud <mhemmatp@cisco.com>
2019-10-31 18:07:18 +0100

    [HICN-357] sysrepo plugin update
    Signed-off-by: masoud <mhemmatp@cisco.com>
    Change-Id: Idabe9d3a3b03139ad3cdb20c8c822e6dd7d4c553

--- scripts/build-sysrepo.sh
+++ scripts/build-sysrepo.sh
@@ -25,12 +25,12 @@ PACKAGECLOUD_RELEASE_REPO_RPM="https://packagecloud.io/install/repositories/fdio
 VPP_GIT_REPO="https://git.fd.io/vpp"
 VPP_BRANCH="stable/1908"
 
-VPP_VERSION_DEB="19.08-release"
-VPP_VERSION_RPM="19.08-release.x86_64"
+VPP_VERSION_DEB="19.08.1-release"
+VPP_VERSION_RPM="19.08.1-release.x86_64"
 
 BUILD_TOOLS_UBUNTU="build-essential doxygen"
 LIBSSL_LIBEVENT_UBUNTU="libevent-dev libssl-dev"
-DEPS_UBUNTU="hicn-light hicn-plugin  libvppinfra=${VPP_VERSION_DEB} libvppinfra-dev=${VPP_VERSION_DEB}  vpp=${VPP_VERSION_DEB} vpp-dev=${VPP_VERSION_DEB} vpp-plugin-core=${VPP_VERSION_DEB}"
+DEPS_UBUNTU="hicn-light libhicn-ctrl libhicn-ctrl-dev  hicn-plugin hicn-plugin-dev libvppinfra=${VPP_VERSION_DEB} libvppinfra-dev=${VPP_VERSION_DEB}  vpp=${VPP_VERSION_DEB} vpp-dev=${VPP_VERSION_DEB} vpp-plugin-core=${VPP_VERSION_DEB}"
 
 # BUILD_TOOLS_GROUP_CENTOS="'Development Tools'"
 DEPS_CENTOS="vpp-devel-${VPP_VERSION_RPM} vpp-lib-${VPP_VERSION_RPM} libparc-devel asio-devel centos-release-scl devtoolset-7"
