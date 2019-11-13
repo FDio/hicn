37cea7d0b96bbf0a01dfdae179cbf7b0dad2f5a4
mashemat <mhemmatp@cisco.com>
2019-11-13 11:48:09 +0100

    [HICN-375] Move cmake in ctrl/sysrepo-plugins to the main cmke in root
    Signed-off-by: mashemat <mhemmatp@cisco.com>
    Change-Id: I6c2f65e61a2f13db8261a32482336b21f07d5e45

--- cmake/Modules/FindHicnPlugin.cmake
+++ cmake/Modules/FindHicnPlugin.cmake
@@ -1,4 +1,5 @@
-#sed under the Apache License, Version 2.0 (the "License");
+# Copyright (c) 2019 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at:
 #
@@ -8,6 +9,7 @@
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
+# limitations under the License.
 # limitations under the License.
 
 set(HICNPLUGIN_SEARCH_PATH_LIST
