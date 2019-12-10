f0299ba9fe52b6028d222861d4c17426daa21876
michele papalini <micpapal@cisco.com>
2019-12-10 13:40:16 +0100

    [HICN-442] DO NOT MERGE new forwarding strategy
    Signed-off-by: michele papalini <micpapal@cisco.com>
    Change-Id: I62c03bddedc83e523fc60f4b50d2c69e38b50318

--- hicn-light/src/hicn/core/connection.h
+++ hicn-light/src/hicn/core/connection.h
@@ -47,8 +47,8 @@ typedef enum {
 #endif /* WITH_POLICY */
 
 // packet types for probing
-#define PACKET_TYPE_PROBE_REQUEST 5
-#define PACKET_TYPE_PROBE_REPLY 6
+//#define PACKET_TYPE_PROBE_REQUEST 5
+//#define PACKET_TYPE_PROBE_REPLY 6
 
 struct connection;
 typedef struct connection Connection;
@@ -156,12 +156,11 @@ const void *connection_Class(const Connection *conn);
 bool connection_ReSend(const Connection *conn, Message *message,
                        bool notification);
 
-void connection_Probe(Connection *conn);
+void connection_Probe(Connection *conn, uint8_t *probe);
 
-void connection_HandleProbe(Connection *conn, uint8_t *message,
-                            Ticks actualTime);
+void connection_HandleProbe(Connection *conn, uint8_t *message);
 
-uint64_t connection_GetDelay(Connection *conn);
+//uint64_t connection_GetDelay(Connection *conn);
 
 void connection_AllowWldrAutoStart(Connection *conn, bool allow);
 
