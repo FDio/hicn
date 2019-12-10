ac8b34e680a4719baf62b416ffb0a1e496912f43
michele papalini <micpapal@cisco.com>
2019-12-10 13:40:16 +0100

    [HICN-442] DO NOT MERGE new forwarding strategy
    Signed-off-by: michele papalini <micpapal@cisco.com>
    Change-Id: I62c03bddedc83e523fc60f4b50d2c69e38b50318
    Signed-off-by: Angelo Mantellini <angelo.mantellini@cisco.com>
    Signed-off-by: michele papalini <micpapal@cisco.com>

--- hicn-light/src/hicn/config/configuration.c
+++ hicn-light/src/hicn/config/configuration.c
@@ -1057,6 +1057,7 @@ struct iovec *configuration_SetForwardingStrategy(Configuration *config,
   struct iovec *response =
       utils_CreateAck(header, control, sizeof(set_strategy_command));
 
+  free((char *) prefix);
   return response;
 }
 
