747ebd7efbd0af0bc166c242a27aecdbc66b27aa
mhemmatp <mhemmatp@cisco.com>
2019-12-12 16:05:13 +0100

    [HICN-440] Add comments to hicn sysrepo plugin code
    Signed-off-by: mhemmatp <mhemmatp@cisco.com>
    Change-Id: I0ed0c5b0b9cd7714b46867e4d4fe0324945cf418

--- ctrl/sysrepo-plugins/hicn-plugin/plugin/hicn_vpp_comm.h
+++ ctrl/sysrepo-plugins/hicn-plugin/plugin/hicn_vpp_comm.h
@@ -13,6 +13,13 @@
  * limitations under the License.
  */
 
+
+/**
+ * @file hicn_vpp_comm.h
+ * @brief This file contains binary api to connect to the VPP.
+ */
+
+
 #ifndef __HICN_VPP_COMMM_H__
 #define __HICN_VPP_COMMM_H__
 #include <sysrepo.h>
@@ -27,15 +34,25 @@
 #endif
 #endif
 
-// ctx vpp connect
+/**
+ * @brief This is the context to connect to the vpp
+ */
+
 extern vapi_ctx_t g_vapi_ctx_instance;
 
-//Here it is the definition
 
+/**
+ * @brief This macro is interface.c to communicate with vpp
+ */
 #define VPP_INTFC_NAME_LEN 64
 #define VPP_MAC_ADDRESS_LEN 8
 #define VPP_IP6_ADDRESS_LEN 16
 
+
+
+/**
+ * @brief This macro checks the arg is NULL or not, if the arg is NULL it returns retval
+ */
 #define ARG_CHECK(retval, arg)                        \
   do {                                                \
     if (NULL == (arg)) {                              \
@@ -45,21 +62,30 @@ extern vapi_ctx_t g_vapi_ctx_instance;
   } while (0)
 
 
-
+/**
+ * @brief Please check ARG_CHECK
+ */
 #define ARG_CHECK2(retval, arg1, arg2) \
     ARG_CHECK(retval, arg1); \
     ARG_CHECK(retval, arg2)
 
-#define ARG_CHECK7(retval, arg1, arg2, arg3, arg4, arg5, arg6, arg7) \
+/**
+ * @brief This Macro is the multiple check of ARG_CHECK
+ */
+#define ARG_CHECK5(retval, arg1, arg2, arg3, arg4, arg5) \
     ARG_CHECK(retval, arg1); \
     ARG_CHECK(retval, arg2); \
     ARG_CHECK(retval, arg3); \
     ARG_CHECK(retval, arg4); \
-    ARG_CHECK(retval, arg5); \
-    ARG_CHECK(retval, arg6); \
-    ARG_CHECK(retval, arg7)
+    ARG_CHECK(retval, arg5)
 
+/**
+ * @brief This function is used to connect to the vpp
+ */
 int hicn_connect_vpp();
+/**
+ * @brief This function is used to close the connection of the vpp
+ */
 int hicn_disconnect_vpp();
 
 #endif  //__HICN_VPP_COMMM_H__
--- ctrl/sysrepo-plugins/hicn-plugin/plugin/model/tlock.h
+++ ctrl/sysrepo-plugins/hicn-plugin/plugin/model/tlock.h
@@ -14,18 +14,51 @@
  */
 
 
+
+/**
+ * @file tlock.h
+ * @brief This file contains ticket lock APIs.
+ */
+
 #ifndef __TLOCK_H__
 #define __TLOCK_H__
 
 
-// limit on the number of locks: it shoud be matched with the  number of hicn-state leaves
+/**
+
+ * @brief limit the number of locks: it shoud be matched with the
+ * number of hicn-state leaves
+ */
 #define MAX_LOCK_SIZE  5
 
-volatile long int  En[MAX_LOCK_SIZE] , De[MAX_LOCK_SIZE] ;   // For Ticket Algorithm
+/**
+ * @brief Ticket lock counters
+ */
+volatile long int  En[MAX_LOCK_SIZE] ;
+
+/**
+ * @brief Ticket lock counters
+ */
+volatile long int De[MAX_LOCK_SIZE] ;
 
 
+/**
+ * @brief This function initialize the ticket lock
+ * @param Lock_Number describes the number of locks need to be initialized
+ * @param init describes the init number
+ */
 void ticket_init ( int Lock_Number , long int init );
+/**
+ * @brief this function acquire the lock
+ * Description of what the function does. This part may refer to the parameters
+ * @param Lock_Number pass the lock
+ */
 void tlock(int Lock_Number );
+/**
+ * @briefthis function release the lock
+ * @param Lock_Number lock number
+
+ */
 void tunlock(int Lock_Number );
 
 #endif /* __IETF_HICN_H__ */
