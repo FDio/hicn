c9e939234463f5c90dfa7d03d16469348601027f
Alberto Compagno <acompagn+fdio@cisco.com>
2020-02-11 08:54:33 +0100

    [HICN-510] Adding collectd plugins to get telemetry from vpp and hicn-plugin
    Change-Id: Idb322dc712b52301c66c5256ad8d1a6a65b503b9
    Signed-off-by: Alberto Compagno <acompagn+fdio@cisco.com>

---
+++ cmake/Modules/FindCollectd.cmake
@@ -0,0 +1,45 @@
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set(COLLECTD_SEARCH_PATH_LIST
+  ${COLLECTD_HOME}
+  $ENV{COLLECTD_HOME}
+  /usr/local
+  /opt
+  /usr
+)
+
+find_path(COLLECTD_INCLUDE_DIR collectd.h
+  HINTS ${COLLECTD_SEARCH_PATH_LIST}
+  PATH_SUFFIXES /include/collectd/core/daemon daemon
+  DOC "Find the collectd includes"
+)
+
+find_path(COLLECTD_COMMON_INCLUDE_DIR common.h
+  HINTS ${COLLECTD_SEARCH_PATH_LIST}
+  PATH_SUFFIXES utils/common/
+  DOC "Find the collectd includes"
+)
+
+find_path(COLLECTD_CONFIG_INCLUDE_DIR config.h
+  HINTS ${COLLECTD_SEARCH_PATH_LIST}
+  DOC "Find the collectd includes"
+)
+
+
+set(COLLECTD_INCLUDE_DIRS ${COLLECTD_INCLUDE_DIR} ${COLLECTD_COMMON_INCLUDE_DIR} ${COLLECTD_CONFIG_INCLUDE_DIR})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(Collectd DEFAULT_MSG COLLECTD_INCLUDE_DIRS)
+
+mark_as_advanced(COLLECTD_INCLUDE_DIRS)
