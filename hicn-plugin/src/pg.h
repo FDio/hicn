9370612e0bd7205f248529045c0ecf856ef844b5
Alberto Compagno <acompagn+fdio@cisco.com>
2020-03-16 15:26:41 +0100

    [HICN-549] Refactored pg to use punting through fib
    - interest packet generator uses the fib to forward interests. The entry in the fib matching the interest name can be a regular IP fib entry or an hICN fib entry. It is up to who configure the forwarder to chose how the forwarding should be done
    - the hicnpg-data is used to keep track of the number of data received in order to have statistics corresponding to the number of interests sent and data received (interest satisfied). This node punt packet registers himself as a node for satisfying local tcp packets (ip4_register_protocol or ip6_register_protocol), therefore tcp packets carrying a local address will be forwarded to it.
    - The hicnpg-server node satisfies interest packets by creating the correspoding data packet. It punts packet by using a feature on the interface that is passed in the configuration. Every packet that arrives on such interface is threated as an hICN interest.
    
    Change-Id: I75ffec4ce0a5afeaf321bcc48dd13fd307dd2925
    Signed-off-by: Alberto Compagno <acompagn+fdio@cisco.com>

--- hicn-plugin/src/pg.h
+++ hicn-plugin/src/pg.h
@@ -27,7 +27,6 @@ typedef struct hicnpg_main_s
   u32 max_seq_number;
   u32 n_flows;
   u32 n_ifaces;
-  u32 hicn_underneath;
   ip46_address_t pgen_clt_src_addr;
   ip46_address_t pgen_clt_hicn_name;
   u16 interest_lifetime;
@@ -38,12 +37,14 @@ extern hicnpg_main_t hicnpg_main;
 typedef struct hicnpg_server_main_s
 {
   u32 node_index;
-  u32 hicn_underneath;
   /* Arbitrary content */
   u32 pgen_svr_buffer_idx;
 } hicnpg_server_main_t;
 
 extern hicnpg_server_main_t hicnpg_server_main;
+
+extern vlib_node_registration_t hicn_pg_interest_node;
+extern vlib_node_registration_t hicn_pg_data_node;
 
 #endif // __HICN_PG_H__
 
