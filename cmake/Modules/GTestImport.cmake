26da97fa97ea203e9c95e723f58c48187bc67144
Mauro Sardara <msardara@cisco.com>
2020-09-07 19:58:45 +0200

    [HICN-636] Add gtest framework and first tests for loop.
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: I133c8adda125c430aa9c4a35fb63bf1d8585afe9
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ cmake/Modules/GTestImport.cmake
@@ -0,0 +1,32 @@
+# Copyright (c) 2020 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+##################################
+# Download and install GoogleTest
+
+include(ExternalProject)
+ExternalProject_Add(gtest
+  URL https://github.com/google/googletest/archive/v1.10.x.zip
+  PREFIX ${CMAKE_BINARY_DIR}/gtest
+  INSTALL_COMMAND ""
+)
+
+ExternalProject_Get_Property(gtest source_dir binary_dir)
+
+message (STATUS "GTest include dir: ${source_dir}/googlemock/include ${source_dir}/googletest/include)")
+message (STATUS "GTest libs: ${binary_dir}/lib/libgmock_main.a ${binary_dir}/lib/libgmock.a ${binary_dir}/lib/libgtest_main.a ${binary_dir}/lib/libgtest.a")
+
+set(GTEST_INCLUDE_DIRS ${source_dir}/googlemock/include ${source_dir}/googletest/include)
+set(GTEST_LIBRARIES ${binary_dir}/lib/libgmock_main.a ${binary_dir}/lib/libgmock.a ${binary_dir}/lib/libgtest_main.a ${binary_dir}/lib/libgtest.a)
+
+enable_testing()
