769f5dc312c9ed9623afa2415e09c35ee6ed3df9
Mauro Sardara <msardara@cisco.com>
2021-02-11 12:04:44 +0100

    HICN-685 Fix package script for centos8
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: I9973034088879b23cc0de6116d65c3a70272a706

--- scripts/functions.sh
+++ scripts/functions.sh
@@ -121,6 +121,9 @@ function install_deps() {
         echo ${DEPS_UBUNTU[@]} | xargs sudo ${apt_get} install -y --allow-unauthenticated --no-install-recommends
     elif [ ${DISTRIB_ID} == "centos" ]; then
         yum config-manager --set-enabled powertools
+        # Temporary workaround until centos fixes the asio-devel package (https://forums.centos.org/viewtopic.php?t=73034)
+        curl -OL http://mirror.centos.org/centos/8/PowerTools/x86_64/os/Packages/asio-devel-1.10.8-7.module_el8.1.0+217+4d875839.x86_64.rpm
+        yum localinstall ./asio-devel-1.10.8-7.module_el8.1.0+217+4d875839.x86_64.rpm
         echo ${DEPS_CENTOS[@]} | xargs sudo yum install -y --nogpgcheck
     fi
 }
