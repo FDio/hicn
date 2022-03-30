3f5e926829d0d3cee403e97a81c5e8a9d3600aea
Luca Muscariello <lumuscar@cisco.com>
2022-03-30 22:29:28 +0200

    The current patch provides several new features, improvements, bug fixes and also     complete rewrite of entire components.
        - lib
    
        The hicn packet parser has been improved with a new packet format fully based on UDP.
        The TCP header is still temporarily supported but the UDP header will replace completely
        the new hicn packet format. Improvements have been made to make sure every packet parsing
        operation is made via this library.
        The current new header can be used as header between the payload and the UDP header
        or as trailer in the UDP suplus area to be tested in the area of UDP options.
    
        - hicn-light
        The portable packet forwarder has been completely rewritten from scratch with
        the twofold objective to improve performance and code size and also drop
        dependencies such as libparc which are now removed by the current implementation.
    
        - hicn control
        the control library is the agent that is used to program the packet forwarders
        via their binary API. This component has benefited from significant improvements
        in terms of interaction model which is now event driven and more robust to failures.
    
        - VPP plugin has been updated to support VPP 22.02
    
        - transport
        Major improvement have been made to the RTC protocol, to the support of IO modules
        and to the security sub system. Signed manifests are the default data authenticity
        and integrity framework. Confidentiality can be enabled by sharing the encryption
        key to the prod/cons layer. The library has been tested with group key based applications
        such as broadcast/multicast and real-time on-line meetings with trusted server keys
        or MLS.
    
    Signed-off-by: Luca Muscariello <lumuscar@cisco.com>
    Change-Id: I75d0ef70f86d921e3ef503c99271216ff583c215

--- hicn-light/src/hicn/utils/punting.h
+++ hicn-light/src/hicn/utils/punting.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017-2019 Cisco and/or its affiliates.
+ * Copyright (c) 2021 Cisco and/or its affiliates.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at:
@@ -16,10 +16,21 @@
 #ifndef punting_h
 #define punting_h
 
+#include <hicn/core/address.h>
+
+typedef struct {
+  char *symbolic;
+  address_t prefix;
+  uint32_t len;
+} punting_t;
+
+#define punting_address(punting) (&((punting)->prefix))
+#define punting_len(punting) ((punting)->len)
+
+#if 0
 struct punting;
 typedef struct punting Punting;
 
-#include <hicn/utils/address.h>
 
 /**
  * Creates a Punting object
@@ -36,7 +47,7 @@ typedef struct punting Punting;
  * @return null An error
  *
  */
-Punting *puntingCreate(const char *symbolic, Address *prefix, uint32_t len);
+Punting *puntingCreate(const char *symbolic, address_t *prefix, uint32_t len);
 
 /**
  * Releases a reference count to the object
@@ -66,7 +77,9 @@ const char *puntingGetSymbolicName(const Punting *punting);
  * Returns the address (INET or INET6 ip address)
  *
  */
-Address *puntingGetAddress(const Punting *punting);
+address_t * puntingGetAddress(const Punting *punting);
 
 uint32_t puntingPrefixLen(const Punting *punting);
+#endif
+
 #endif  // punting_h
