2122dd771275132125438fdf980ccbe5c34d8c97
Luca Muscariello <muscariello@ieee.org>
2022-08-10 14:53:21 +0200

    feat: Sonarcloud
    Signed-off-by: Luca Muscariello <muscariello@ieee.org>
    Change-Id: I9796ca60932bfb85fc827a1050a60e9209f827e6

--- scripts/build-sonar.sh
+++ scripts/build-sonar.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+export SONAR_TOKEN=$API_TOKEN
 export SONAR_SCANNER_VERSION=4.7.0.2747
 export SONAR_SCANNER_HOME=$HOME/.sonar/sonar-scanner-$SONAR_SCANNER_VERSION-linux
 curl --create-dirs -sSLo $HOME/.sonar/sonar-scanner.zip https://binaries.sonarsource.com/Distribution/sonar-scanner-cli/sonar-scanner-cli-$SONAR_SCANNER_VERSION-linux.zip
