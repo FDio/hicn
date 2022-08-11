25f6cb3100ae397125cf071579036b9f3d4487b6
Mauro Sardara <msardara@cisco.com>
2022-08-11 18:44:10 +0200

    ci: install docker-compose as part of sonarcloud scan
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: I2216084a415abbb874dfa49051ca3ca0fcb7c41d

--- scripts/build-sonar.sh
+++ scripts/build-sonar.sh
@@ -13,4 +13,7 @@
 
 #!/bin/bash
 
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
 docker-compose -f docker-compose-sonar.yaml up
