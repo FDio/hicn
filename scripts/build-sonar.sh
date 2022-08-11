a42b58605819b74e2002d67e913b241e4ddb15e1
Mauro Sardara <msardara@cisco.com>
2022-08-11 19:01:39 +0200

    ci(sonarscan): fix typo
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: Ibadcc7b1a79ef92149b032becc800f1ed8ba00c5

--- scripts/build-sonar.sh
+++ scripts/build-sonar.sh
@@ -16,4 +16,4 @@
 sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
-docker-compose -f docker-compose-sonar.yaml up
+docker-compose -f docker-compose-sonar.yml up
