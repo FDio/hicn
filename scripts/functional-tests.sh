10aa7112a09e0331772746ab85a00a7ff4244078
Mauro Sardara <msardara@cisco.com>
2022-08-16 09:43:05 +0200

    ci(functional-tests): add functional tests scripts
    Ref: HICN-732
    
    Signed-off-by: Mauro Sardara <msardara@cisco.com>
    Change-Id: Ib945cf7bd0ee331e33270cf1005dfebd085862eb
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ scripts/functional-tests.sh
@@ -0,0 +1,28 @@
+# Copyright (c) 2017-2022 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -euxo pipefail
+
+env
+
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+source "${SCRIPT_PATH}/functions.sh"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+functional_test
+
+exit 0
