78d2daceaebd207811bc1c12a7a16a5d384b224c
Mauro Sardara <msardara@cisco.com>
2022-08-16 09:43:05 +0200

    ci(functional-tests): add functional tests scripts
    - Add scripts/functional-tests.sh script
    - Add `download_artifacts` functions, allowing to retrieve artifacts of a gerrit patch
    - Robot: redirect output of topology build to files, thus preventing robot from hanging
    - Share folders between host and containers with the :z flag
    - Use sudo to install executable in docker-compose scripts, as the user of the docker is not root anymore
    - Makefile: decouple installation and build
    
    Ref: HICN-732
    
    Change-Id: Ib945cf7bd0ee331e33270cf1005dfebd085862eb
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ scripts/functional-tests.sh
@@ -0,0 +1,34 @@
+# Copyright (c) 2017-2022 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#!/bin/bash
+
+set -euxo pipefail
+
+SCRIPT_PATH=$( cd "$(dirname "${BASH_SOURCE}")" ; pwd -P )
+source "${SCRIPT_PATH}/functions.sh"
+
+echo "---------------------------------------------------------"
+echo "-----  INSTALLING FUNCTIONAL TEST DEPENDENCIES  ---------"
+echo "---------------------------------------------------------"
+
+# This docker compose version is well supported also in CentOS
+DOCKER_COMPOSE_VERSION=1.29.2
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+sudo pip3 install robotframework
+
+functional_test
+
+exit 0
