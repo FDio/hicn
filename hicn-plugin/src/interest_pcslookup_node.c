d5cc5a43eb62a354eb6c4b713241ff1b12805438
Mauro Sardara <msardara@cisco.com>
2022-08-26 15:02:12 +0000

    feat(hicn-plugin): interest manifest
    Ref: HICN-748
    
    Change-Id: Ie403de53a93094dca997cec379db6f5d3ce8e6be
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- hicn-plugin/src/interest_pcslookup_node.c
+++ hicn-plugin/src/interest_pcslookup_node.c
@@ -44,10 +44,47 @@ static char *hicn_interest_pcslookup_error_strings[] = {
 };
 
 vlib_node_registration_t hicn_interest_pcslookup_node;
+{
+  int ret = HICN_ERROR_NONE;
+  hicn_pcs_entry_t pcs_entry;
+
+  uint32_t *results = NULL;
+  u32 clone[1];
+  vlib_buffer_t *b0 = NULL;
+  u32 next0;
+
+  // Deserialize interest manifest
+  interest_manifest_deserialize (int_manifest_header);
+
+  if (!interest_manifest_is_valid (int_manifest_header, int_manifest_size))
+    return;
+
+  // Iterate over interest manifest entries
+  interest_manifest_foreach_suffix (int_manifest_header, name.suffix)
+  {
+    // Lookup name
+    ret = hicn_pcs_lookup_one (pitcs, &name, &pcs_entry);
+
+    if (ret == HICN_ERROR_NONE)
+      {
+	// Match in PCS. We need to clone a packet for to the
+	// interest_hic{pit,cs} nodes.
+	vlib_buffer_clone (vm, bi0, clone, 1, 0);
+
+	// Get clone and set the PCS result
+	b0 = vlib_get_buffer (vm, bi0);
+	hicn_get_buffer (b0)->pcs_entry_id =
+	  hicn_pcs_entry_get_index (pitcs, pcs_entry);
+
+	// Send to next
+	next0 = HICN_INTEREST_PCSLOOKUP_NEXT_INTEREST_HITPIT +
+		hicn_pcs_entry_is_cs (pcs_entry);
+      }
+  }
+}
 
 /*
- * ICN forwarder node for interests: handling of Interests delivered based on
- * ACL. - 1 packet at a time - ipv4/tcp ipv6/tcp
+ * ICN forwarder node for interests.
  */
 static uword
 hicn_interest_pcslookup_node_fn (vlib_main_t *vm, vlib_node_runtime_t *node,
@@ -170,6 +207,171 @@ hicn_interest_pcslookup_node_fn (vlib_main_t *vm, vlib_node_runtime_t *node,
   return (frame->n_vectors);
 }
 
+/*
+ * ICN forwarder node for interests manifest
+ */
+static uword
+hicn_interest_manifest_pcslookup_node_fn (vlib_main_t *vm,
+					  vlib_node_runtime_t *node,
+					  vlib_frame_t *frame)
+{
+  int ret;
+  u32 n_left_from, *from, *to_next;
+  hicn_interest_pcslookup_next_t next_index;
+  hicn_interest_pcslookup_runtime_t *rt;
+  vl_api_hicn_api_node_stats_get_reply_t stats = { 0 };
+  vlib_buffer_t *b0;
+  u32 bi0;
+  u32 next0 = HICN_INTEREST_PCSLOOKUP_NEXT_ERROR_DROP;
+  hicn_pcs_entry_t *pcs_entry = NULL;
+  interest_manifest_header_t *int_manifest_header = NULL;
+  int pos = 0;
+
+  rt = vlib_node_get_runtime_data (vm, hicn_interest_pcslookup_node.index);
+
+  if (PREDICT_FALSE (rt->pitcs == NULL))
+    {
+      rt->pitcs = &hicn_main.pitcs;
+    }
+  from = vlib_frame_vector_args (frame);
+  n_left_from = frame->n_vectors;
+  next_index = node->cached_next_index;
+
+  u32 clone[1];
+
+  while (n_left_from > 0)
+    {
+      u32 n_left_to_next;
+      vlib_get_next_frame (vm, node, next_index, to_next, n_left_to_next);
+      while (n_left_from > 0 && n_left_to_next > 0)
+	{
+	  /* Prefetch for next iteration. */
+	  if (n_left_from > 1)
+	    {
+	      vlib_buffer_t *b1;
+	      b1 = vlib_get_buffer (vm, from[1]);
+	      CLIB_PREFETCH (b1, CLIB_CACHE_LINE_BYTES, STORE);
+	    }
+
+	  // Dequeue a packet buffer
+	  bi0 = from[0];
+	  from += 1;
+	  n_left_from -= 1;
+
+	  b0 = vlib_get_buffer (vm, bi0);
+
+	  // By default we send the interest to strategy node
+	  next0 = HICN_INTEREST_PCSLOOKUP_NEXT_STRATEGY;
+
+	  // Update stats
+	  stats.pkts_processed++;
+
+	  // Check if the interest is in the PCS already
+	  hicn_name_t name;
+	  hicn_packet_get_name (&hicn_get_buffer (b0)->pkbuf, &name);
+
+	  ASSERT (hicn_buffer_get_payload_type (b0) == HPT_MANIFEST);
+
+	  // Process interest manifest
+	  u8 *payload;
+	  size_t payload_size;
+	  hicn_interest_get_payload (&hicn_get_buffer (b0)->pkbuf, &payload,
+				     &payload_size, 0);
+	  interest_manifest_header_t *int_manifest_header =
+	    (interest_manifest_header_t *) (payload);
+
+	  // Deserialize interest manifest
+	  interest_manifest_deserialize (int_manifest_header);
+
+	  if (interest_manifest_is_valid (int_manifest_header, payload_size))
+	    {
+	      interest_manifest_foreach_suffix (int_manifest_header,
+						name.suffix, pos)
+	      {
+		ret = hicn_pcs_lookup_one (rt->pitcs, &name, &pcs_entry);
+
+		if (ret == HICN_ERROR_NONE)
+		  {
+		    // Match in PCS. We need to clone a packet for to the
+		    // interest_hic{pit,cs} nodes.
+		    vlib_buffer_clone (vm, bi0, clone, 1, 0);
+
+		    next0 = HICN_INTEREST_PCSLOOKUP_NEXT_INTEREST_HITPIT +
+			    hicn_pcs_entry_is_cs (pcs_entry);
+
+		    ret = hicn_store_internal_state (
+		      clone, hicn_pcs_entry_get_index (rt->pitcs, pcs_entry),
+		      vnet_buffer (b0)->ip.adj_index[VLIB_TX]);
+
+		    if (PREDICT_FALSE (ret != HICN_ERROR_NONE))
+		      next0 = HICN_INTEREST_PCSLOOKUP_NEXT_ERROR_DROP;
+
+		    to_next[0] = clone[0];
+		    to_next += 1;
+		    n_left_to_next -= 1;
+
+		    // Distinguish between aggregation or retransmission
+		    ret = hicn_pcs_entry_pit_search (
+		      pcs_entry, hicn_get_buffer (b0)->face_id);
+		    if (!ret)
+		      {
+			bitmap_unset_no_check (
+			  int_manifest_header->request_bitmap, pos);
+		      }
+
+		    // Maybe trace
+		    if (PREDICT_FALSE ((node->flags & VLIB_NODE_FLAG_TRACE) &&
+				       (b0->flags & VLIB_BUFFER_IS_TRACED)))
+		      {
+			hicn_interest_pcslookup_trace_t *t =
+			  vlib_add_trace (vm, node, b0, sizeof (*t));
+			t->pkt_type = HICN_PACKET_TYPE_INTEREST;
+			t->sw_if_index =
+			  vnet_buffer (b0)->sw_if_index[VLIB_RX];
+			t->next_index = next0;
+		      }
+		    /*
+		     * Verify speculative enqueue, maybe switch current
+		     * next frame
+		     */
+		    vlib_validate_buffer_enqueue_x1 (vm, node, next_index,
+						     to_next, n_left_to_next,
+						     bi0, next0);
+		  }
+		else
+		  {
+		    // No match. Forward interest to strategy node.
+		    forward = 1;
+		  }
+	      }
+	    }
+	  else
+	    {
+	      next0 = HICN_INTEREST_PCSLOOKUP_NEXT_ERROR_DROP;
+	    }
+	}
+      vlib_put_next_frame (vm, node, next_index, n_left_to_next);
+    }
+
+  u32 pit_int_count = hicn_pcs_get_pit_count (rt->pitcs);
+  u32 pit_cs_count = hicn_pcs_get_cs_count (rt->pitcs);
+
+  vlib_node_increment_counter (vm, hicn_interest_pcslookup_node.index,
+			       HICNFWD_ERROR_PROCESSED, stats.pkts_processed);
+
+  vlib_node_increment_counter (vm, hicn_interest_pcslookup_node.index,
+			       HICNFWD_ERROR_INTERESTS,
+			       stats.pkts_interest_count);
+
+  update_node_counter (vm, hicn_interest_pcslookup_node.index,
+		       HICNFWD_ERROR_INT_COUNT, pit_int_count);
+
+  update_node_counter (vm, hicn_interest_pcslookup_node.index,
+		       HICNFWD_ERROR_CS_COUNT, pit_cs_count);
+
+  return (frame->n_vectors);
+}
+
 /* packet trace format function */
 static u8 *
 hicn_interest_pcslookup_format_trace (u8 *s, va_list *args)
@@ -184,6 +386,18 @@ hicn_interest_pcslookup_format_trace (u8 *s, va_list *args)
   return (s);
 }
 
+VLIB_NODE_FN (hicn_interest_pcslookup_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+  return hicn_interest_pcslookup_node_fn (vm, node, frame);
+}
+
+VLIB_NODE_FN (hicn_interest_manifest_pcslookup_node)
+(vlib_main_t *vm, vlib_node_runtime_t *node, vlib_frame_t *frame)
+{
+  return hicn_interest_manifest_pcslookup_node_fn (vm, node, frame);
+}
+
 /*
  * Node registration for the interest forwarder node
  */
@@ -191,6 +405,29 @@ VLIB_REGISTER_NODE(hicn_interest_pcslookup_node) =
 {
   .function = hicn_interest_pcslookup_node_fn,
   .name = "hicn-interest-pcslookup",
+  .vector_size = sizeof(u32),
+  .runtime_data_bytes = sizeof(hicn_interest_pcslookup_runtime_t),
+  .format_trace = hicn_interest_pcslookup_format_trace,
+  .type = VLIB_NODE_TYPE_INTERNAL,
+  .n_errors = ARRAY_LEN(hicn_interest_pcslookup_error_strings),
+  .error_strings = hicn_interest_pcslookup_error_strings,
+  .n_next_nodes = HICN_INTEREST_PCSLOOKUP_N_NEXT,
+  .next_nodes =
+  {
+    [HICN_INTEREST_PCSLOOKUP_NEXT_STRATEGY] = "hicn-strategy",
+    [HICN_INTEREST_PCSLOOKUP_NEXT_INTEREST_HITPIT] = "hicn-interest-hitpit",
+    [HICN_INTEREST_PCSLOOKUP_NEXT_INTEREST_HITCS] = "hicn-interest-hitcs",
+    [HICN_INTEREST_PCSLOOKUP_NEXT_ERROR_DROP] = "error-drop",
+  },
+};
+
+/*
+ * Node registration for the interest manifest forwarder node
+ */
+VLIB_REGISTER_NODE(hicn_interest_manifest_pcslookup_node) =
+{
+  .function = hicn_interest_pcslookup_node_fn,
+  .name = "hicn-interest-manifest-pcslookup",
   .vector_size = sizeof(u32),
   .runtime_data_bytes = sizeof(hicn_interest_pcslookup_runtime_t),
   .format_trace = hicn_interest_pcslookup_format_trace,
