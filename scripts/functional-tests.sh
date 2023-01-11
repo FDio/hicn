fbe593aa657fe80f19e799612671c4fc8d7d8abd
Mauro Sardara <msardara@cisco.com>
2023-01-11 17:08:16 +0000

    feat: add default branch to gerrit configuratioon
    Change-Id: Ib7c43ae5f9dd7cdf4de3559088c8055d55c24b73
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- scripts/functional-tests.sh
+++ scripts/functional-tests.sh
@@ -26,9 +26,9 @@ echo "---------------------------------------------------------"
 # Versions >= 2.0.0 do not seem to work.
 DOCKER_COMPOSE_VERSION=1.29.2
 
-sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-sudo pip3 install robotframework
+# sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+# sudo chmod +x /usr/local/bin/docker-compose
+# sudo pip3 install robotframework
 
 functional_test
 
