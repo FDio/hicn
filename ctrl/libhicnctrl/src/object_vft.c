74b76a9a4d4a1716babdea7bcb8c801e7112755a
Jordan Augé <jordan.auge+fdio@cisco.com>
2023-01-24 01:46:29 +0100

    feat: libhicnctrl: new API to allow sending MAP-Me command on specific face
    Change-Id: Iac39a1d145a5c1b4f19e1fa35d713ca720393760
    Ticket: HICN-832
    Signed-off-by: Jordan Augé <jordan.auge+fdio@cisco.com>

--- ctrl/libhicnctrl/src/object_vft.c
+++ ctrl/libhicnctrl/src/object_vft.c
@@ -4,19 +4,21 @@
 #include "objects/connection.h"
 #include "objects/route.h"
 #include "objects/face.h"
+#include "objects/mapme.h"
 #include "objects/stats.h"
 #include "objects/strategy.h"
 #include "objects/subscription.h"
 #include "objects/active_interface.h"
 
 const hc_object_ops_t *object_vft[] = {
-    [OBJECT_TYPE_LISTENER] = &hc_listener_ops,
     [OBJECT_TYPE_CONNECTION] = &hc_connection_ops,
+    [OBJECT_TYPE_LISTENER] = &hc_listener_ops,
     [OBJECT_TYPE_ROUTE] = &hc_route_ops,
     [OBJECT_TYPE_FACE] = &hc_face_ops,
-    [OBJECT_TYPE_FACE_STATS] = &hc_face_stats_ops,
-    [OBJECT_TYPE_STATS] = &hc_stats_ops,
     [OBJECT_TYPE_STRATEGY] = &hc_strategy_ops,
+    [OBJECT_TYPE_MAPME] = &hc_mapme_ops,
     [OBJECT_TYPE_SUBSCRIPTION] = &hc_subscription_ops,
     [OBJECT_TYPE_ACTIVE_INTERFACE] = &hc_active_interface_ops,
+    [OBJECT_TYPE_STATS] = &hc_stats_ops,
+    [OBJECT_TYPE_FACE_STATS] = &hc_face_stats_ops,
 };
